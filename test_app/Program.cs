c35d3bf11f8b479daf810fa8553d0aceac802505
Vladimir Vasiltsov <vvasiltsov@evolenthealth.com>
2014-01-09 16:10:58 -0500

    sample usage.

--- test_app/Program.cs
+++ test_app/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using Interfaces;
 
 namespace test_app
 {
@@ -10,6 +7,40 @@ namespace test_app
     {
         static void Main(string[] args)
         {
+            var i = System.Console.Out.GoIWriteLine();
+            Print(i);
+        }
+
+        public static void Print(IWriteLine wl)
+        {
+            wl.WriteLine("some text");
+        }
+
+    }
+
+    public static class Extensions
+    {
+        public static IWriteLine GoIWriteLine(this TextWriter tw)
+        {
+            return new TextWriterGoFacade(tw);
         }
     }
+
+    public class TextWriterGoFacade : IWriteLine
+    {
+        private readonly TextWriter _obj;
+
+        public TextWriterGoFacade(TextWriter obj)
+        {
+            _obj = obj;
+        }
+
+        public void WriteLine(string format, params object[] args)
+        {
+            _obj.WriteLine(format,args);
+        }
+
+    }
+
+
 }
